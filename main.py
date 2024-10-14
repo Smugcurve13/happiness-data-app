68e37292e23b51bb4197813efe4f50e7ea4e3ac5
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-14 12:03:18 +0530

    alt done

--- main.py
+++ main.py
@@ -2,14 +2,34 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv('happy.csv')
-
 st.title("In Search For Happiness")
 
-xaxis = st.selectbox("Select the Data for X-Axis",['gdp','happiness','generosity'])
-yaxis = st.selectbox("Select the Data for Y-Axis",['gdp','happiness','generosity'])
+xaxis = st.selectbox("Select the Data for X-Axis",['GDP','Happiness','Generosity'])
+yaxis = st.selectbox("Select the Data for Y-Axis",['GDP','Happiness','Generosity'])
 
-st.subheader(f'{xaxis.title()} and {yaxis.title()}')
+df = pd.read_csv('happy.csv')
 
-figure = px.scatter(df, x=xaxis,y=yaxis,labels={'x':xaxis.title(),'y':yaxis.title})
+
+match xaxis:
+    case "GDP":
+        x_array = df['gdp']
+    case "Happiness":
+        x_array = df['happiness']    
+    case "Generosity":
+        x_array = df['generosity']
+
+match yaxis:
+    case "GDP":
+        y_array = df['gdp']
+    case "Happiness":
+        y_array = df['happiness']    
+    case "Generosity":
+        y_array = df['generosity']
+
+print(xaxis,yaxis)
+
+
+st.subheader(f'{xaxis} and {yaxis}')
+
+figure = px.scatter(df, x=x_array,y=y_array,labels={'x':xaxis,'y':yaxis})
 st.plotly_chart(figure)
